b59343fa5464a573deb429b16144312e8f00988f
marimotona <ssmmo.06021224@gmail.com>
2023-06-21 21:00:17 +0900

    1.2

---
+++ chapter1/1.2.py
@@ -0,0 +1,65 @@
+# apple , alepp -> true
+
+def check_string(s1:str, s2:str) -> bool:
+    if len(s1) != len(s2):
+        return False
+    
+    map = {}
+    for i in range(len(s1)):
+        if s1[i] not in map:
+            map[s1[i]] = 1
+        else:
+            map[s1[i]] += 1
+    print(map)
+
+    for i in range(len(s2)):
+        print(s2[i])
+        if s2[i] not in map:
+            print(s2[i])
+            return False
+        
+    return True
+        
+    
+
+# print(check_string('apple', 'alepp'))
+# print(check_string('apple', 'appla'))
+
+def check_string2(s1:str, s2:str) -> bool:
+    if len(s1) != len(s2):
+        return False
+    
+    new_s1, new_s2 = sorted(s1), sorted(s2)
+
+    for i in range(len(s1)):
+        if new_s1[i] != new_s2[i]:
+            return False
+        
+    return True
+
+print(check_string2('apple', 'alepp'))
+print(check_string2('apple', 'appla'))
+
+def check_string3(s1:str, s2:str) -> bool:
+    if len(s1) != len(s2):
+        return False
+    
+    map = {}
+    for i in range(len(s1)):
+        if s1[i] not in map:
+            map[s1[i]] = 1
+        else:
+            map[s1[i]] += 1
+    
+    for i in range(len(s2)):
+        if s2[i] not in map:
+            return False
+        else:
+            map[s2[i]] -= 1
+            if map[s2[i]] == 0:
+                del map[s2[i]]
+
+    return len(map) == 0
+    
+print(check_string3('apple', 'alepp'))
+print(check_string3('apple', 'appla'))

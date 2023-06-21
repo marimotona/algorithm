ef59a094ab53496ad4c4860eaa3975fdbd9231ce
marimotona <ssmmo.06021224@gmail.com>
2023-06-22 08:32:48 +0900

    create file

--- chapter1/1.6.py
+++ chapter1/1.6.py
@@ -21,3 +21,25 @@ def count_alpha(s:str) -> str:
 print(count_alpha('aabbaa'))
 print(count_alpha('aabbc'))
 print(count_alpha('aaBC'))
+
+
+# string builder
+
+def count_alpha_build(s:str) -> str:
+    count = 1
+    result = []
+
+    for i in range(1, len(s)):
+        if s[i - 1] == s[i]: # i = 1 / b / a
+            count += 1 # count = 1
+        else:
+            result.append(s[i - 1] + str(count))
+            count = 1
+
+    result.append(s[-1] + str(count))
+
+    return ''.join(result)
+
+print(count_alpha_build('aabbaa'))
+print(count_alpha_build('aabbc'))
+print(count_alpha_build('aaBC'))

15b59cd8a9879e94b72f8aca8a3e8c405aad2185
marimotona <ssmmo.06021224@gmail.com>
2023-06-22 22:09:58 +0900

    2.6

--- chapter2/2.6.py
+++ chapter2/2.6.py
@@ -39,3 +39,33 @@ anagram_list = CreateLinkedList.create_linkedlist([7, 1, 6, 5, 6, 1, 7])
 result = is_anagram(anagram_list)
 # PrintLinkedList.print_linkedlist(result)
 print(result)
+
+
+def is_palid(node):
+    first = node
+    slow = node
+
+    stack = []
+
+    while first is not None and first.next is not None:
+        stack.append(slow.value)
+        slow = slow.next
+        first = first.next.next
+
+    if first is not None:
+        slow = slow.next
+
+    while slow is not None:
+        value = stack.pop()
+
+        if value != slow.value:
+            return False
+        
+        slow = slow.next
+
+    return True
+
+anagram_list = CreateLinkedList.create_linkedlist([7, 1, 6, 5, 6, 1, 7])
+result = is_palid(anagram_list)
+# PrintLinkedList.print_linkedlist(result)
+print(result)

27e0e34c6b9fe5825d9711ff7603c0234b3e7884
marimotona <ssmmo.06021224@gmail.com>
2023-06-22 11:38:31 +0900

    2.1

--- chapter2/2.2.py
+++ chapter2/2.2.py
@@ -2,41 +2,41 @@ from linkedlist import LinkedListNode, CreateLinkedList, PrintLinkedList
 
 # 1,2,3,4,5 k = 3 / output:3
 
-def k_node(node, k):
-    curr = node
-    arr = []
+# def k_node(node, k):
+#     curr = node
+#     arr = []
 
-    while curr:
-        arr = curr
-        curr= curr.next
+#     while curr:
+#         arr = curr
+#         curr= curr.next
 
-    result = arr[-k]
+#     result = arr[-k]
 
-    return result
-
-
-
-node1 = LinkedListNode(1)
-node2 = LinkedListNode(2)
-node3 = LinkedListNode(3)
-node4 = LinkedListNode(6)
-node5 = LinkedListNode(7)
-
-# ノードを連結
-node1.next = node2
-node2.next = node3
-node3.next = node4
-node4.next = node5
+#     return result
 
 
-def print_linked_list(node):
-    curr = node
-    while curr:
-        print(curr.value, end=" ")
-        curr = curr.next
-    print()
+def k_node(head, k):
+    p1 = head # fast
+    p2 = head # slow
 
-result_list = k_node(node1, 2)
-print_linked_list(result_list)
+    for i in range(k):
+        if not p1:
+            return None
+        else:
+            p1 = p1.next
+
+    while p1:
+        p1 = p1.next
+        p2 = p2.next
+
+    return p2
+
+
+
+arr = [1,2,3,6,7,8,9]
+linkedlist = CreateLinkedList.create_linkedlist(arr)
+result_k = k_node(linkedlist, 3)
+# PrintLinkedList.print_linkedlist(deleted_list)
+print(result_k)
 
 

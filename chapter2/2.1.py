47a18ab3c3891ed04a76caf8de69d0d6955301b3
marimotona <ssmmo.06021224@gmail.com>
2023-06-22 10:47:13 +0900

    linkedlist

---
+++ chapter2/2.1.py
@@ -0,0 +1,37 @@
+from linkedlist import LinkedListNode
+
+def delete_list(node):
+    curr = node
+    prev = None
+    seen = set()
+
+    while curr:
+        if curr.value in seen:
+            prev.next = curr.next
+        else:
+            seen.add(curr.value)
+            prev = curr
+        curr = curr.next
+
+    return node
+
+node1 = LinkedListNode(1)
+node2 = LinkedListNode(2)
+node3 = LinkedListNode(3)
+node4 = LinkedListNode(2)
+
+# ノードを連結
+node1.next = node2
+node2.next = node3
+node3.next = node4
+
+def print_linked_list(node):
+    curr = node
+    while curr:
+        print(curr.value, end=" ")
+        curr = curr.next
+    print()
+
+deleted_list = delete_list(node1)
+
+print_linked_list(deleted_list)

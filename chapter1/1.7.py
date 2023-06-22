7670c0346e5639e368da26aac220be5089239be7
marimotona <ssmmo.06021224@gmail.com>
2023-06-22 09:37:55 +0900

    1.7

---
+++ chapter1/1.7.py
@@ -0,0 +1,48 @@
+'''
+
+% # # # &      @ # # # %
+# # # # #      # # # # #
+# # # # #  ->  # # # # #
+# # # # #      # # # # #
+@ # # # !      ! # # # &
+
+
+'''
+
+def rotate(matrix):
+    n = len(matrix)
+    print(n)
+    print(matrix)
+
+    for i in range(n):
+        for j in range(i, n):
+            temp = matrix[i][j]
+            matrix[i][j] = matrix[j][i]
+            matrix[j][i] = temp
+
+    for i in range(n):
+        print(n/2)
+        for j in range(int(n /2)):
+            temp = matrix[i][j]
+            matrix[i][j] = matrix[i][n- 1 - j]
+            matrix[i][n- 1 - j] = temp
+
+    return matrix
+
+
+# print(rotate([[1, 2, 3], [4, 5, 6], [7, 8, 9]]))
+
+
+def rotate_matrix(matrix):
+    n = len(matrix)
+
+    for i in range(n):
+        for j in range(i, n):
+            matrix[j][i], matrix[i][j] = matrix[i][j], matrix[j][i]
+
+    for i in range(n):
+        matrix[i] = matrix[i][::-1]
+
+    return matrix
+
+print(rotate_matrix([[1, 2, 3], [4, 5, 6], [7, 8, 9]]))

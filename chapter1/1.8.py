0eb19efba5e297768b010b58966983cbba641603
marimotona <ssmmo.06021224@gmail.com>
2023-06-25 19:05:28 +0900

    re 1.8

--- chapter1/1.8.py
+++ chapter1/1.8.py
@@ -11,6 +11,21 @@
 '''
 
 def zero_matrix(matrix):
+    # m, n = len(matrix), len(matrix[0])
+    # rows, cols = set(), set()
+
+    # for i in range(m):
+    #     for j in range(n):
+    #         if matrix[i][j] == 0:
+    #             rows.add(i)
+    #             cols.add(j)
+
+    # for i in range(m):
+    #     for j in range(n):
+    #         if i in rows or j in cols:
+    #             matrix[i][j] = 0
+
+    # return matrix
     m, n = len(matrix), len(matrix[0])
     rows, cols = set(), set()
 
@@ -26,6 +41,5 @@ def zero_matrix(matrix):
                 matrix[i][j] = 0
 
     return matrix
-
 
 print(zero_matrix([[1,0,3,4,5],[6,7,8,9,10],[11,12,13,14,15]]))

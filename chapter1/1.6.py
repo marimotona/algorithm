aab5e258e3f3ff94f37c0c4fd6669525b5342e98
marimotona <ssmmo.06021224@gmail.com>
2023-06-25 18:41:12 +0900

    1.6

--- chapter1/1.6.py
+++ chapter1/1.6.py
@@ -43,3 +43,22 @@ def count_alpha_build(s:str) -> str:
 print(count_alpha_build('aabbaa'))
 print(count_alpha_build('aabbc'))
 print(count_alpha_build('aaBC'))
+
+
+def alpha_build(s:str) -> str:
+    result = ''
+    count = 1
+    for i in range(1, len(s)):
+        if s[i] == s[i - 1]:
+            count += 1
+        else:
+            result += (s[i - 1] + str(count))
+            count = 1
+
+    result += (s[-1] + str(count))
+
+    return result
+
+print(alpha_build('aabbaa'))
+print(alpha_build('aabbc'))
+print(alpha_build('aaBC'))
